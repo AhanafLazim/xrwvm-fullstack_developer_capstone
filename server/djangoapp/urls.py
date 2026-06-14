c78bc4204ae9836d1337800e977d161a71fcf9c5
AhanafLazim <lazingoku@gmail.com>
2026-06-14 09:29:57 -0400

    Add register, login and logout functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,11 +2,13 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    path(route='register', view=views.registration, name='register'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
